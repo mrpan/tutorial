02077083cc501a53c01ac2e45557dfc3429f2f41
giserpan <543421341@qq.com>
2019-04-03 18:16:23 +0800

    add ReferencingFactoryFinder example

---
+++ src/main/java/org/geotools/tutorial/GeotoolsTest.java
@@ -0,0 +1,96 @@
+package org.geotools.tutorial;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
+import org.geotools.data.DataAccess;
+import org.geotools.data.DataAccessFinder;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureLockFactory;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFactorySpi;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.filter.FunctionFactory;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.referencing.ReferencingFactoryFinder;
+import org.geotools.referencing.factory.ReferencingFactoryContainer;
+import org.geotools.styling.StyleFactory;
+import org.opengis.feature.Feature;
+import org.opengis.feature.FeatureFactory;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.FeatureTypeFactory;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory;
+import org.opengis.filter.expression.Function;
+import org.opengis.referencing.cs.CSFactory;
+import org.opengis.referencing.datum.DatumAuthorityFactory;
+import org.opengis.referencing.datum.DatumFactory;
+
+public class GeotoolsTest {
+
+	public GeotoolsTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		FilterFactory filterFactory=CommonFactoryFinder.getFilterFactory();
+		Filter filter = filterFactory.less( filterFactory.property( "size" ), filterFactory.literal( 2 ) );
+		//org.geotools.styling.StyleFactory
+		StyleFactory styleFactory =CommonFactoryFinder.getStyleFactory();
+		
+		Set<Function> funcset= CommonFactoryFinder.getFunctions(null);
+		Set<FileDataStoreFactorySpi> fdstore=CommonFactoryFinder.getFileDataStoreFactories(null);
+		//org.opengis.feature.FeatureFactory
+		FeatureFactory featureFactory=(FeatureFactory) CommonFactoryFinder.getFeatureFactory(null);
+		Object[] array=null;
+		SimpleFeatureType type=null;
+		String id=null;
+//		SimpleFeature sf=featureFactory.createSimpleFeature(array, type, id);
+		
+		FeatureTypeFactory ftFactory=CommonFactoryFinder.getFeatureTypeFactory(null);
+//		SimpleFeatureType sftype=ftFactory.createSimpleFeatureType(null, null, null, (Boolean) false, null, null, null);
+		
+		FeatureCollections fc=CommonFactoryFinder.getFeatureCollections();
+		
+		File file = new File("example.shp");
+		Map map = new HashMap();
+		map.put( "url", file.toURL() );
+//		DataAccess<FeatureType,Feature> dataAccess=DataAccessFinder.getDataStore(null);
+		
+		DataStore dataStore=DataStoreFinder.getDataStore(map);
+		String[] typenames=dataStore.getTypeNames();
+		
+		FileDataStore fileDataStore=FileDataStoreFinder.getDataStore(file);
+		FileDataStore fileDataStore2=FileDataStoreFinder.getDataStore(file.toURL());
+//		SimpleFeatureType featuretype= fileDataStore.getSchema();
+//		SimpleFeatureSource sfSource=fileDataStore.getFeatureSource();
+		//org.locationtech.jts.geom.GeometryFactory
+		GeometryFactory geometryFactory= JTSFactoryFinder.getGeometryFactory();
+		Coordinate cd=null;
+//		Point p=geometryFactory.createPoint(cd);
+
+		PrecisionModel pm =geometryFactory.getPrecisionModel();
+		System.out.println(pm.getType());
+
+		DatumFactory datumFactory= ReferencingFactoryFinder.getDatumFactory(null);
+		CSFactory csFactory=ReferencingFactoryFinder.getCSFactory(null);
+
+		DatumAuthorityFactory datumAuthorityFactory=ReferencingFactoryFinder.getDatumAuthorityFactory("EPSG",null);
+		System.out.println(datumAuthorityFactory.getAuthority());
+	}
+
+}
